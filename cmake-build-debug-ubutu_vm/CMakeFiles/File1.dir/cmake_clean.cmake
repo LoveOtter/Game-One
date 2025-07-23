81071a20b561f86cee7a5985c7ef628cc17cd624
LoveOtter <2244143381@qq.com>
2025-07-23 21:31:31 +0800

    2025/7/23

---
+++ cmake-build-debug-ubutu_vm/CMakeFiles/File1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/File1.dir/main.cpp.o"
+  "CMakeFiles/File1.dir/main.cpp.o.d"
+  "File1"
+  "File1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/File1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-ubutu_vm/CMakeFiles/File1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for File1.
+# This may be replaced when dependencies are built.
